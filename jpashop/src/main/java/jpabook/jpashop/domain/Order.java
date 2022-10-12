6b59cc351fa0c6f444a1f3479b94859f384d6343
SonDongHwan <110151129+DonghwanSon1@users.noreply.github.com>
2022-10-12 19:20:26 +0900

    cascade=CascadeType.All

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -21,10 +21,10 @@ public class Order {
     @JoinColumn(name = "member_id") //맵핑을 뭘로 할건지 정하기 위해 씀
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private  Delivery delivery;
 
