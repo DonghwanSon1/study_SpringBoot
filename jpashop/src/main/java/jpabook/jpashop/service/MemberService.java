5bef4c5fb199114cd1988b955d61ad316c65b606
SonDongHwan <110151129+DonghwanSon1@users.noreply.github.com>
2022-10-13 20:39:52 +0900

    회원 서비스

---
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,58 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) // 읽기 전용으로 해놓고 쓰기를 해야되는경우 @Transactional만 적으면 우선권을 가져 쓰기용으로 할수 있음
+@RequiredArgsConstructor //final이 적힌 것만 생성자를 만들어준다.
+public class MemberService {
+
+    private final MemberRepository memberRepository; //final 넣는걸 추천한다. 그래야 생성자에서 빠진게 있는지 확인할 수 있어서
+
+    //@Autowired 생성자가 하나인 경우 안써도 자동으로 대입해준다.
+//    public MemberService(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+    /**
+     * 회원가입
+     */
+    @Transactional
+    public Long join(Member member){
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        //EXCEPTION
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    //회원 전체 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+
+
+
+
+
+
+
+
+}
