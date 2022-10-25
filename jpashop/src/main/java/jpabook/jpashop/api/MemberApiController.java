3b7425324f9937934959ada2219d3ab8caae8bd9
SonDongHwan <110151129+DonghwanSon1@users.noreply.github.com>
2022-10-25 14:37:08 +0900

    JPA2편 api 시작

---
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller @ResponseBody => 합친게 @RestController
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    
+
+
+
+
+
+
+
+}
