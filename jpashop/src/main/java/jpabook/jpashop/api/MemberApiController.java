ba72c456f8c449fa24c05f0838252f16352e2c36
SonDongHwan <110151129+DonghwanSon1@users.noreply.github.com>
2022-10-27 21:03:11 +0900

    회원 조회 API

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -4,6 +4,7 @@ import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import java.sql.Statement;
 import java.util.List;
+import java.util.stream.Collectors;
 
 //@Controller @ResponseBody => 합친게 @RestController
 @RestController
@@ -34,7 +36,7 @@ public class MemberApiController {
      */
 
 
-    //회원 등록
+    //회원 등록 (안좋은 API)
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
         Long id = memberService.join(member);
@@ -81,11 +83,40 @@ public class MemberApiController {
      */
     //조회 V1: 안 좋은 버전, 모든 엔티티가 노출, @JsonIgnore -> 이건 정말 최악, api가 이거 하나인가! 화면에 종속적이지 마라!
 
-    //회원 조회
+    //회원 조회 (안좋은 API)
     @GetMapping("/api/v1/members")
     public List<Member> membersV1(){
         return memberService.findMembers();
     }
+
+    //회원 조회
+    @GetMapping("/api/v2/members")
+    public Result memberV2(){
+        List<Member> findMembers = memberService.findMembers();
+
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String name;
+    }
+
+
+
+
 
     //별도의(회원 등록) DTO
     @Data
