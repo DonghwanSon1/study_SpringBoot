febda9b9cf8bab2d0545c4dd998079ab85f9b8a6
SonDongHwan <110151129+DonghwanSon1@users.noreply.github.com>
2022-10-31 19:17:41 +0900

    간단한 주문 조회 3 (엔티티를 직접 노출)

--- jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -18,7 +18,6 @@ import java.util.List;
  * Order -> Delivery
  *
  */
-// findAllByString 안됌; 다시 해보기
 @RestController
 @RequiredArgsConstructor
 public class OrderSimpleApiController {
@@ -26,10 +25,19 @@ public class OrderSimpleApiController {
     /** * V1. 엔티티 직접 노출
      * - Hibernate5Module 모듈 등록, LAZY=null 처리
      * - 양방향 관계 문제 발생 -> @JsonIgnore
+     *
+     * 주의 : 지연 로딩(LAZY)을 피하기 위해 즉시 로딩(EARGR)으로 설정하면 안된다! 즉시 로딩 때문에
+     * 연관관계가 필요 없는 경우에도 데이터를 항상 조회해서 성능 문제가 발생할 수 있다. 즉시 로딩으로
+     * 설정하면 성능 튜닝이 매우 어려워 진다.
+     * > 항상 지연 로딩을 기본으로 하고, 성능 최적화가 필요한 경우에는 페치 조인(fetch join)을 사용해라!(V3에서 설명)
      */
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        for(Order order : all){
+            order.getMember().getName(); //Lazy 강제 초기화
+            order.getDelivery().getAddress(); //Lazy 강제 초기화
+        }
         return all;
     }
 }
