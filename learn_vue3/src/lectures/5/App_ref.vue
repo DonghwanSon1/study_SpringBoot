63b6e716b983d21664707b9d59d20576575af143
SonDongHwan <110151129+DonghwanSon1@users.noreply.github.com>
2023-03-23 19:30:29 +0900

    반응형 기초(Reactivity)

---
+++ learn_vue3/src/lectures/5/App_ref.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<p>{{ message }}</p>
+		<button v-on:click="addMessage">add Click</button>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		// let message = reactive('Hello Vue3');
+		// 위에처럼 할 시 반응형이 안된다. -> reactive는 반응형 함수인데 반응형 함수는 객체로 선언을 해줘야 가능하다.
+		// 즉 위에처럼 말고, reactive({ value: 'Hello Vue3' }) 이렇게 해야된다.
+		// 따라서, addMessage도 message.value += '!'; 을 써주면 된다.
+		// 하지만 이런 단점을 보안하기 위해 ref()함수가 있다.
+		let message = ref('Hello Vue3!');
+		const addMessage = () => {
+			message.value += '!';
+		};
+		console.log(message);
+		console.log('message: ', message.value);
+		console.log('message typeof: ', typeof message.value);
+		return {
+			message,
+			addMessage,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ learn_vue3/src/lectures/5/App_toRefs_toRef.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<p>author: {{ author }}</p>
+		<p>title: {{ title }}</p>
+	</div>
+</template>
+
+<script>
+import { reactive, toRef } from 'vue';
+
+export default {
+	setup() {
+		const book = reactive({
+			author: 'Vue Team',
+			year: '2020',
+			title: 'Vue 3 Guide',
+			description: '당신은 지금 바로 이 책을 읽습니다. :)',
+			price: '무료',
+		});
+		// const { author, title } = book;
+		// console.log(typeof author);
+		// console.log(typeof book);
+		// 위에처럼 했을 시 반응형을 잃어 버린다. -> string이다.
+		// 따라서 반응형을 않잃어버리게 하려면 toRefs를 사용해야된다.
+
+		// const { author, title } = toRefs(book); -> 두개 이상 가져오고 싶을때!
+		const author = toRef(book, 'author');
+		const title = toRef(book, 'title');
+
+		return {
+			author,
+			title,
+			book,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
