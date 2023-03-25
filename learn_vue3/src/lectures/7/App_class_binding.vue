14c3c4d9e83894f3700ed55f175e71a88ccd8ed2
SonDongHwan <110151129+DonghwanSon1@users.noreply.github.com>
2023-03-25 15:32:14 +0900

    class_binding/style_binding

---
+++ learn_vue3/src/lectures/7/App_class_binding.vue
@@ -0,0 +1,59 @@
+<template>
+	<div>
+		<div
+			class="text"
+			:class="[isActive ? 'active-class' : 'class', errorClass, classObject]"
+		>
+			텍스트 입니다.
+		</div>
+		<button @click="toggle">toggle</button>
+		<button @click="hasError = !hasError">toggleError</button>
+	</div>
+</template>
+
+<script>
+import { ref, computed } from 'vue';
+
+export default {
+	setup() {
+		const isActive = ref(true);
+		const hasError = ref(false);
+		// const classObject = reactive({
+		// 	active: true,
+		// 	'text-danger': false,
+		// });
+
+		const classObject = computed(() => {
+			return {
+				active: true && true,
+				'text-danger': true && true,
+				'text-blue': true,
+			};
+		});
+
+		const activeClass = ref('active');
+		const errorClass = ref('error');
+
+		const toggle = () => {
+			isActive.value = !isActive.value;
+		};
+		return {
+			isActive,
+			toggle,
+			hasError,
+			classObject,
+			activeClass,
+			errorClass,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.active {
+	font-weight: 900;
+}
+.text-danger {
+	color: red;
+}
+</style>
---
+++ learn_vue3/src/lectures/7/App_style_binding.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<div :style="styleObject">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+			placeat repellendus recusandae, dolorem porro veniam, esse atque,
+			obcaecati voluptates molestiae ducimus aut veritatis inventore mollitia
+			quos alias aliquid adipisci aliquam.
+		</div>
+		<button @click="fontSize--">-</button>
+		<button @click="fontSize++">+</button>
+	</div>
+</template>
+
+<script>
+import { computed, ref } from 'vue';
+
+export default {
+	setup() {
+		// const styleObject = reactive({
+		// 	color: 'red',
+		// 	fontSize: '13px',
+		// }); // 인라인 스타일은 카멜표기법으로 해야된다.
+
+		const fontSize = ref(13);
+		const styleObject = computed(() => {
+			return {
+				color: 'red',
+				fontSize: fontSize.value + 'px',
+			};
+		});
+		return {
+			styleObject,
+			fontSize,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
