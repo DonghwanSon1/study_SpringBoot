caa37bd137312b7b466639167f4e50df788bb473
SonDongHwan <110151129+DonghwanSon1@users.noreply.github.com>
2023-04-02 20:14:38 +0900

    immediate

---
+++ learn_vue3/src/lectures/13/App_immediate.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<p>{{ message }}</p>
+		<p>{{ reversMessage }}</p>
+	</div>
+</template>
+
+<script>
+import { ref, watch } from 'vue';
+
+export default {
+	setup() {
+		const message = ref('Hello Vue3');
+		const reversMessage = ref('');
+
+		const reverseFunction = () => {
+			console.log('즉시 실행함!!');
+			reversMessage.value = message.value.split('').reverse().join('');
+		};
+		watch(message, reverseFunction);
+		reverseFunction();
+
+		return {
+			message,
+			reversMessage,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
