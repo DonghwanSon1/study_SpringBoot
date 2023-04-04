c276184a89846ffcd6ec33c78fa15afaef8dcbab
SonDongHwan <110151129+DonghwanSon1@users.noreply.github.com>
2023-04-04 21:51:04 +0900

    Single-File Component

---
+++ learn_vue3/src/components/TheView.vue
@@ -0,0 +1,42 @@
+<template>
+	<main>
+		<div class="container py-4">
+			<div class="row g-3">
+				<div class="col col-4">
+					<AppCard></AppCard>
+					<!-- <app-card></app-card>도 됨 -->
+				</div>
+				<div class="col col-4">
+					<AppCard></AppCard>
+				</div>
+				<div class="col col-4">
+					<AppCard></AppCard>
+				</div>
+				<div class="col col-4">
+					<AppCard></AppCard>
+				</div>
+				<div class="col col-4">
+					<AppCard></AppCard>
+				</div>
+				<div class="col col-4">
+					<AppCard></AppCard>
+				</div>
+			</div>
+		</div>
+	</main>
+</template>
+
+<script>
+import AppCard from './AppCard.vue';
+export default {
+	template: {
+		AppCard,
+	},
+	setup() {
+		return {};
+	},
+	components: { AppCard },
+};
+</script>
+
+<style lang="scss" scoped></style>
