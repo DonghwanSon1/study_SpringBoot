02ece7ad8b15fd09dd6263e66d0addd33ebbdd87
SonDongHwan <110151129+DonghwanSon1@users.noreply.github.com>
2023-04-05 19:55:09 +0900

    components 정리

---
+++ learn_vue3/src/components/SFC/AppCard.vue
@@ -0,0 +1,63 @@
+<!-- 다음 강좌를 위해 일부로 카피함, 여기 코드를 돌리고 싶으면 (SFC)를 지우고 실행하면됨 -->
+<template>
+	<div class="card">
+		<div class="card-body">
+			<h5 class="card-title red">Card title</h5>
+			<p class="card-text">
+				Some quick example text to build on the card title and make up the bulk
+				of the card's content.
+			</p>
+			<a href="#" class="btn btn-primary">Go somewhere</a>
+		</div>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		// const style = useCssModule();
+		// console.log('style: ', style);
+		console.log('AppCard setup()');
+
+		const color = ref('red');
+		color.value = 'blue';
+		return {
+			color,
+		};
+	},
+};
+</script>
+
+<!-- <style module>
+.red {
+	color: red !important;
+}
+</style> -->
+
+<style>
+.red {
+	color: v-bind(color) !important;
+}
+</style>
+
+<!-- <style lang="scss" scoped>
+.red {
+	color: red !important;
+}
+</style>  
+-> scoped는 현재 이 파일에서만 style이 변경된다.-->
+
+<!-- <style module>
+.red {
+	color: red !important;
+}
+</style> 
+-> 모듈로 사용할 시 $style.클래스명 으로 모듈화 할 수 있다.-->
+
+<!-- <style module="classes">
+.red {
+	color: red !important;
+}
+</style> -->
