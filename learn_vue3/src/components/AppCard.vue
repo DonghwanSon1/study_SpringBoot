80f1bb9c88791ffdf198080ebb209ea3efb55aba
SonDongHwan <110151129+DonghwanSon1@users.noreply.github.com>
2023-04-11 21:57:12 +0900

    slot(non-prop 커밋한거랑 섞임)

--- learn_vue3/src/components/AppCard.vue
+++ learn_vue3/src/components/AppCard.vue
@@ -1,71 +1,33 @@
 <template>
 	<div class="card">
-		<div class="card-body">
-			<!-- type: news, notice -->
-			<span class="badge bg-secondary">{{ typeName }}</span>
-			<h5 class="card-title red mt-2">{{ title }}</h5>
-			<p class="card-text">
-				{{ contents }}
-			</p>
-			<a href="#" class="btn" :class="isLikeClass" @click="toggleLike">
-				좋아요
-			</a>
+		<div v-if="$slots.header" class="card-header">
+			<slot name="header" header-message="헤더 메시지"></slot>
+		</div>
+		<!-- {{ $slots }} -->
+		<div v-if="$slots.default" class="card-body">
+			<slot :child-message="childMessage" hello-message="안녕하신가요?!"></slot>
+		</div>
+		<div v-if="hasFooter" class="card-footer text-body-secondary">
+			<slot name="footer" footer-message="푸터 메시지"></slot>
 		</div>
 	</div>
 </template>
 
 <script>
-import { computed } from 'vue';
+import { ref, computed } from 'vue';
 
 export default {
-	// props: ['title', 'contents'], -> 기본 props의 선언 방식
-	props: {
-		type: {
-			type: String,
-			default: 'news',
-			validator: value => {
-				return ['news', 'notice'].includes(value);
-			},
-		},
-		title: {
-			type: String,
-			required: true,
-		},
-		contents: {
-			type: String,
-			// required: true,
-		},
-		isLike: {
-			type: Boolean,
-			default: false,
-		},
-		obj: {
-			type: Object,
-			default: () => ({}),
-		},
-	},
-	emits: ['toggleLike'],
-	setup(props, context) {
-		// console.log('props.title: ', props.title);
-		const isLikeClass = computed(() =>
-			props.isLike ? 'btn-danger' : 'btn-outline-danger',
+	setup(props, { slots }) {
+		const childMessage = ref(
+			'하위(자식) 컴포넌트의 데이터는 상위에서 사용할 수 없습니다!',
 		);
-		const typeName = computed(() =>
-			props.type === 'news' ? '뉴스' : '공지사항',
-		);
-
-		const toggleLike = () => {
-			// props.isLike = !props.isLike; //자식에서 엄마의 값을 변경할 순 없다. 변경하고 싶을땐 아래의 방식으로는 가능하다.
-			// props.obj.title = '김길동'; //JavaScript 언어 특징상 억지로 변경은 가능함.
-			context.emit('toggleLike');
-		};
+		const hasFooter = computed(() => !!slots.footer);
 		return {
-			isLikeClass,
-			typeName,
-			toggleLike,
+			childMessage,
+			hasFooter,
 		};
 	},
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped></style>
